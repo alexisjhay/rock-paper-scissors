eec98f8d0a75cbebd7feeba2d98e912f8fead9c0
Alexis Jhay Rosales <50823115+alexisjhay@users.noreply.github.com>
2026-08-28 21:54:16 +0800

    refactor: update getComputerChoice function

--- javascript/script.js
+++ javascript/script.js
@@ -3,15 +3,9 @@ let computerScore = 0;
 
 
 function getComputerChoice(){
-    let randomGenerator = Math.floor(Math.random()* 3) + 1;
-
-    if(randomGenerator === 1){
-        return "rock"
-    }else if(randomGenerator === 2){
-        return "paper"
-    }else if(randomGenerator === 3){
-        return "scissors"
-    }
+    const computerChoice = ["rock", "paper", "scissors"]
+    let randomGenerator = Math.floor(Math.random()* computerChoice.length -1) + 1;
+    return computerChoice[randomGenerator];
 }
 
 function getHumanChoice(){
